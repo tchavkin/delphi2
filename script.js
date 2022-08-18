b66f668009649f6148fa1aa13fd81fa9433f175c
Troy Chavkin <tachavkin@gmail.com>
2022-08-18 14:27:00 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,57 @@
 function renderTime(offset) {
 
-  var currentTime = new Date();
-  currentTime.setHours(currentTime.getHours()+9);
-  var diem = "AM";
-  var h = currentTime.getHours();
-  var m = currentTime.getMinutes();
-  var s = currentTime.getSeconds();
-  
-  
-  if (h == 0) {
-      h = 12
-  } else if (h > 12) {
-      h = h - 12;
-      diem = "PM";
-  }
-  
-  if (h < 10) {
-      h = "0" + h;
-  }
-  
-  if (m < 10) {
-      m = "0" + m;
-  }
-  
-  if (s < 10) {
-      s = "0" + s;
-  }
-  
-  var myClock = document.getElementById ("clockDisplay");
-  myClock.textContent = h + ":" + m + ":" + s + " " + diem;
-  
-  
-  setTimeout ('renderTime()', 1000);
-  
-  }
-  
-  renderTime(-2);
+var currentTime = new Date();
+currentTime.setHours(currentTime.getHours()+9);
+var diem = "AM";
+var h = currentTime.getHours();
+var m = currentTime.getMinutes();
+var s = currentTime.getSeconds();
 
-
-function test() {
-console.log("hover")
+if (h == 0) {
+    h = 12
+  } 
+else if (h > 12) {
+    h = h - 12;
+    diem = "PM";
+  }
+if (h < 10) {
+    h = "0" + h;
+  }
+if (m < 10) {
+    m = "0" + m;
+  }
+if (s < 10) {
+    s = "0" + s;
+  }
+  
+var myClock = document.getElementById ("clockDisplay");
+myClock.textContent = h + ":" + m + ":" + s + " " + diem;
+setTimeout ('renderTime()', 1000);
 }
+  
+renderTime(-2);
 
-function test2() {
-    console.log("unhover")
+
+function mainPicDefault() {
+    document.getElementById("mainpic").innerHTML = "<img src=\"assets/panorama-athena-pronaia.png\" width=\"900vh\" height=\"465vh\">"
 }
 
 function carouselOracle() {
-    document.getElementById("mainpic").innerHTML = "<img src=\"assets/oracle-of-delphi.png\" width=\"1000vh\" height=\"505vh\">"
+    document.getElementById("mainpic").innerHTML = "<img src=\"assets/oracle-of-delphi.png\" width=\"900vh\" height=\"465vh\">"
 }
 
 function carouselTemple() {
-    document.getElementById("mainpic").innerHTML = "<img src=\"assets/the-temple-of-apollo.png\" width=\"1000vh\" height=\"505vh\">"
+    document.getElementById("mainpic").innerHTML = "<img src=\"assets/the-temple-of-apollo.png\" width=\"900vh\" height=\"465vh\">"
 }
 
 function carouselTholos() {
-    document.getElementById("mainpic").innerHTML = "<img src=\"assets/tholos-of-delphi.png\" width=\"1000vh\" height=\"505vh\">"
+    document.getElementById("mainpic").innerHTML = "<img src=\"assets/tholos-of-delphi.png\" width=\"900vh\" height=\"465vh\">"
 }
 
 function carouselTheatre() {
-    document.getElementById("mainpic").innerHTML = "<img src=\"assets/theatre-of-delphi.png\" width=\"1000vh\" height=\"505vh\">"
+    document.getElementById("mainpic").innerHTML = "<img src=\"assets/theatre-of-delphi.png\" width=\"900vh\" height=\"465vh\">"
 }
 
 function carouselStadium() {
-    document.getElementById("mainpic").innerHTML = "<img src=\"assets/stadium-of-delphi-3.png\" width=\"1000vh\" height=\"505vh\">"
+    document.getElementById("mainpic").innerHTML = "<img src=\"assets/stadium-of-delphi-3.png\" width=\"900vh\" height=\"465vh\">"
 }
